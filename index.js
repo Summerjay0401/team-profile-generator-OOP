1dad7321a3d62a41639c32335a3697fcab838d32
Summer Jade <summerjadefrosal@gmail.com>
2022-08-05 01:04:35 -0400

    added templatehelper

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const fs = require('fs/promises');
 const inquirer =require('inquirer');
 
-const { generateHTML } = require('./utils/generateHTML.js');
+const templateHelper = require('./src/templateHelper');
 
 const questions = [
     {
         type: "list",
-        message: "Do you want to add a member on your team?",
-        name: "teamMember",
+        message: "Select Role",
+        name: "role",
         choices:[
             "Manager", 
             "Engineer", 
             "Intern", 
-            "Done adding team members", 
         ]
     },
     {
@@ -33,28 +32,50 @@ const questions = [
     {
         type: "input",
         message: "What is your Office Number?",
-        name: "officeNumber",
+        name: "officeNumber", 
+        when: (answers) => answers.role === 'Manager'
     },
     {
         type: "input",
         message: "What is your GitHub username?",
         name: "gitUsername",
+        when: (answers) => answers.role === 'Engineer'
     }, 
     {
         type: "input",
         message: "What is the name of your School?",
         name: "schoolName",
+        when: (answers) => answers.role === 'Intern'
     }, 
+    {
+        type: "confirm",
+        name: "is_finished",
+        message: "Do you want to add more team members?",
+      },
 ]
+
+let employees = [];
+
+const getAnswers = () => {
+    return inquirer.prompt (questions).then((answers)=> {
+        employees.push(answers);
+        if (answers.is_finished === false) {
+            return employees;
+        }
+        else {
+            return getAnswers();
+        }
+    }); 
+}
 
 function init() {
 
-        inquirer.prompt (questions).then(({teamMember,name,idNumber,emailAddress,officeNumber,gitUsername,schoolName})=> {
-
-        const myTeam = generateHTML({teamMember,name,idNumber,emailAddress,officeNumber,gitUsername,schoolName});
-
-        writeToFile("dist/index.html", myTeam);
-    });    
+    getAnswers()
+    .then((employees) => templateHelper(employees))
+    .catch((error) => {});
+   
 }
 
 init();
+
+// {role,name,idNumber,emailAddress,officeNumber,gitUsername,schoolName,is_finished}
